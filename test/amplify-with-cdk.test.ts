2c467afd2c15f9e207340ad46e8269586eb2a279
Tomasz Łakomy <tomasz@stedi.com>
2023-01-12 20:16:07 +0100

    Initial commit

---
+++ test/amplify-with-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmplifyWithCdk from '../lib/amplify-with-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amplify-with-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmplifyWithCdk.AmplifyWithCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
